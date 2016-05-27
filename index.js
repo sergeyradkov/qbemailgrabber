0870156c29e67a3cd1aa7c317475bc4dbcccc7d9
sergeyradkov <radkovsn@gmail.com>
2016-05-26 19:37:12 -0600

    2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express =require('express')
 , port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
+
 server.listen(port, function(){
     console.log('Running.. ')
 })
