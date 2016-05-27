9bff2825faf5f95db6d8dac39d0f85809892b684
sergeyradkov <radkovsn@gmail.com>
2016-05-27 13:42:14 -0600

    123

--- public/app/views/customer-controller.js
+++ public/app/views/customer-controller.js
@@ -1,37 +1,33 @@
 angular.module('QB_Project')
-    .controller('CustomerController', function($scope, recaptchaService) {
-        
+    .controller('CustomerController', function ($scope, recaptchaService) {
+
         var cc = this;
         cc.updatedCustomer = {};
-        
-        cc.addCustomer = function(customer) {
+
+        cc.addCustomer = function (customer) {
             cc.updatedCustomer = customer;
             console.log(cc.updatedCustomer);
-            cc.newCustomer = {};
+            if (grecaptcha.getResponse()) {
+                customer.captchaResponse = grecaptcha.getResponse(); //This will add the response string to the object you are sending to your server so you can make your get request server side to verify
+                recaptchaService.sendForm(customer).then(function (response) {
+                    window.response = response.data;
+                    cc.newCustomer = {};
+                });
+            } else {
+                $scope.error = "Please Verify you are not a robot";
+            }
         }
-    
-    $scope.submitForm = function(item){
-    if(grecaptcha.getResponse()){
-      item.captchaResponse = grecaptcha.getResponse(); //This will add the response string to the object you are sending to your server so you can make your get request server side to verify
-      recaptchaService.sendForm(item).then(function(response){
-        $scope.response = response.data;
-        $scope.item = '';
-        });
-        } else {
-        $scope.error = "Please Verify you are not a robot";
-        }
-    };
-})
+    })
 
-    .service('recaptchaService', function($http){
-    return {
-    sendForm: function (item) {
-        console.log(item);
-        return $http({
-        method: 'POST',
-        url: '/',
-        data: item
-        });
-    }
-    }
+    .service('recaptchaService', function ($http) {
+        return {
+            sendForm: function (item) {
+                console.log(item);
+                return $http({
+                    method: 'POST',
+                    url: '/',
+                    data: item
+                });
+            }
+        }
     });
