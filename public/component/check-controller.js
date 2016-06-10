fa4e06646a997098f8d1d685a8cffc46451376cb
sergeyradkov <radkovsn@gmail.com>
2016-06-09 19:13:57 -0600

    123

---
+++ public/component/check-controller.js
@@ -0,0 +1,70 @@
+
+
+angular.module('qbhelper').component('customerComponent', {
+    templateUrl: '/views/check/check.html',
+    controllerAs: 'ch',
+    controller: function ($scope, $state, MemberService, PhoneService) {
+
+        var ch = this;
+
+        ch.formatPhoneNumber = function (phoneNumber) {
+            debugger
+            ch.phoneNumber = PhoneService.formatPhoneNumber(phoneNumber)
+        }
+
+        function handleServerSuccess(res) {
+            if (res.data) {
+                ch.checked = true;
+                ch.currentCustomer = res.data;
+                ch.currentCustomer.phoneNumber = res.data.PrimaryPhone.FreeFormNumber;
+                debugger
+                //   $state.go('customer', { id: res.data.id })
+            } else {
+                console.log(" ERROR ");
+                $('#myModal').modal('show');
+            }
+        }
+
+        function handleServerError(err) {
+
+        }
+
+        ch.find = function (phoneNumber) {
+            var normalNumber = phoneNumber;//.replace (/[^\d]/g, "");
+            MemberService.findMemberByPhone(normalNumber).then(handleServerSuccess, handleServerError);
+
+        };
+
+        ch.updateCustomer = function (customer) {
+
+            ch.updatedCustomer = customer;
+            
+            console.log(ch.updatedCustomer);
+
+            if (grecaptcha.getResponse()) {
+                customer.captchaResponse = grecaptcha.getResponse(); //This will add the response string to the object you are sending to your server so you can make your get request server side to verify
+                recaptchaService.sendForm(customer).then(function (response) {
+                    window.response = response.data;
+                    ch.currentCustomer = {};
+                });
+            } else {
+                $scope.error = "Please Verify you are not a robot";
+            }
+        }
+
+    }
+});
+
+(function () {
+    angular.module("qbhelper").service('recaptchaService', function ($http) {
+        return {
+            sendForm: function (item) {
+                return $http({
+                    method: 'POST',
+                    url: '/',
+                    data: item
+                });
+            }
+        }
+    });
+})();
