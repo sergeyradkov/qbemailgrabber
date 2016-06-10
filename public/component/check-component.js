b73e8c4e2d5b1be5e8aa846704719f28dbfa526c
sergeyradkov <radkovsn@gmail.com>
2016-06-10 15:36:42 -0600

    312

--- public/component/check-component.js
+++ public/component/check-component.js
@@ -16,31 +16,27 @@ angular.module('qbhelper').component('customerComponent', {
             if (res.data) {
                 ch.checked = true;
                 ch.currentCustomer = res.data;
-                console.log(ch.currentCustomer);
-            //    ch.currentCustomer.phoneNumber = res.data.PrimaryPhone.FreeFormNumber;
-                debugger
             } else {
-                console.log(" ERROR ");
-                $('#myModal').modal('show');
+                console.log(" WRONG PHONE NUMBER ");
+                $('#phoneWrong').modal('show');
             }
         }
 
         function handleServerError(err) {
-
+                console.log("SERVER ERROR ");
+                $('#serverError').modal('show');
         }
 
         ch.find = function (phoneNumber) {
             var normalNumber = phoneNumber;//.replace (/[^\d]/g, "");
             MemberService.findMemberByPhone(normalNumber).then(handleServerSuccess, handleServerError);
-
         };
 
-        ch.updateCustomer = function (customer) {
-
-            ch.updatedCustomer = customer;
+        ch.updateCustomer = function (updatedCustomer) {
+            ch.updatedCustomer = updatedCustomer;
             
             console.log(ch.updatedCustomer);
-
+            debugger
             if (grecaptcha.getResponse()) {
                 customer.captchaResponse = grecaptcha.getResponse(); //This will add the response string to the object you are sending to your server so you can make your get request server side to verify
                 recaptchaService.sendForm(customer).then(function (response) {
