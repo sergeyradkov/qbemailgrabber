797e930775f819ea2aa50e80745c9800573792e3
sergeyradkov <radkovsn@gmail.com>
2016-06-14 17:01:51 -0600

    123

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var http = require('http'),
   express = require('express'),
   app = express(),
   port = process.env.PORT || 8080
-request = require('request'),
+  request = require('request'),
   bodyParser = require('body-parser'),
   qs = require('querystring'),
   util = require('util'),
   cookieParser = require('cookie-parser'),
   session = require('express-session'),
-  QuickBooks = require('./index')
-captchaUrl = 'https://www.google.com/recaptcha/api/siteverify?secret=6LeWCCETAAAAAGtTk0MKqtHyPEyNZtfRpqND-uV1&response='
+  QuickBooks = require('./index'),
+  captchaUrl = 'https://www.google.com/recaptcha/api/siteverify?secret=6LeWCCETAAAAAGtTk0MKqtHyPEyNZtfRpqND-uV1&response='
 
 // GENERIC EXPRESS CONFIG
 app.use(express.static(__dirname + '/public'))
@@ -165,4 +165,47 @@ app.listen(port, function () {
   console.log('Express server listening on port ' + app.get('port'))
 })
 
+// TWILIO PART
 
+var ACCOUNT_SID = '';
+var AUTH_TOKEN = '';
+var myNumber = '+12085059247';
+var sendNumber = '+12082839080';
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
+
+//Send an SMS text message
+client.sendMessage({
+
+    to: sendNumber, // Any number Twilio can deliver to
+    from: myNumber, // A number you bought from Twilio and can use for outbound communication
+    body: 'word to your mother.' // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+    }
+});
+
+// //Place a phone call, and respond with TwiML instructions from the given URL
+// client.makeCall({
+
+//     to:'+16515556677', // Any number Twilio can call
+//     from: '+14506667788', // A number you bought from Twilio and can use for outbound communication
+//     url: 'http://www.example.com/twiml.php' // A URL that produces an XML document (TwiML) which contains instructions for the call
+
+// }, function(err, responseData) {
+
+//     //executed when the call has been initiated.
+//     console.log(responseData.from); // outputs "+14506667788"
+
+// });
