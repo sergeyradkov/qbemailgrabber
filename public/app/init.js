e3c032e4514a5fc85166ce390010f3174ff89674
sergeyradkov <radkovsn@gmail.com>
2016-06-21 15:43:22 -0600

    12

--- public/app/init.js
+++ public/app/init.js
@@ -1,1 +1,7 @@
-(function(){angular.module("qbhelper",['ui.router'])}());
+(function(){
+    angular.module("qbhelper",['ui.router', 'js-data']);
+    angular.module("qbhelper")
+        .run(function(DS, DSHttpAdapter) {
+        DS.registerAdapter('http', DSHttpAdapter, { default: true });
+        })
+}());
